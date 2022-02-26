65aea8271c96ea92d9732a2dd8c116a7dbfb7b76
dgh <tjln0608@gmail.com>
2022-02-26 22:55:52 +0900

    init

---
+++ src/test/java/com/dgh/basicmock/BasicMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgh.basicmock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/dgh/basicmock/controller/RouterTest.java
@@ -0,0 +1,69 @@
+package com.dgh.basicmock.controller;
+
+import com.dgh.basicmock.config.route.info.LocalRoutingInfoLoader;
+import com.dgh.basicmock.config.route.info.RoutingInfo;
+import com.dgh.basicmock.config.route.info.RoutingInfoLoader;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@WebFluxTest(controllers = Router.class)
+public class RouterTest {
+
+    @SpyBean
+    private RoutingInfo routingInfo;
+
+    @SpyBean
+    private MockOperation mockOperation;
+
+    @SpyBean
+    private RoutingHandler routingHandler;
+
+    @SpyBean
+    private LocalRoutingInfoLoader localRoutingInfoLoader;
+
+    @SpyBean
+    private WebTestClient webTestClient;
+
+    @Test
+    @DisplayName("[ Routing default - GET /test/ok ] 200 OK")
+    void routeOkResponseTest() {
+
+        // When & Then
+        webTestClient.get().uri("/test/ok")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk();
+
+    }
+
+    @Test
+    @DisplayName("[ Routing - GET /test/404 ] 404 NOT Found")
+    void routeNotFoundResponseTest() {
+
+        // When & Then
+        webTestClient.get().uri("/test/404")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isNotFound()
+                .expectBody(String.class)
+                .isEqualTo("Not Found");
+    }
+
+    @Test
+    @DisplayName("[ Routing - GET /test/500 ] 500 Internal error")
+    void routeInternalErrorResponseTest() {
+
+        // When & Then
+        webTestClient.get().uri("/test/500")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().is5xxServerError()
+                .expectBody(String.class)
+                .isEqualTo("Internal error");
+    }
+
+}
