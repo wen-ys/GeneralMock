e15d12a87052aa754dfc874878218c477c096924
dgh <tjln0608@gmail.com>
2022-02-27 09:40:23 +0900

    Change package name

--- src/main/java/com/dgh/gerneralmock/GeneralMockApplication.java
+++ src/main/java/com/dgh/gerneralmock/GeneralMockApplication.java
@@ -6,9 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @Slf4j
-public class GenernalMockApplication {
+public class GeneralMockApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GenernalMockApplication.class, args);
+        SpringApplication.run(GeneralMockApplication.class, args);
     }
 }
