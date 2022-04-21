3d3743fb90f58d70d1ff0702d2cf90944c600729
dgh <tjln0608@gmail.com>
2022-04-21 17:30:08 +0900

    Add body log

--- src/main/java/com/wenys/gerneralmock/controller/RoutingHandler.java
+++ src/main/java/com/wenys/gerneralmock/controller/RoutingHandler.java
@@ -3,8 +3,10 @@ package com.wenys.gerneralmock.controller;
 import com.wenys.gerneralmock.config.filter.Filter;
 import com.wenys.gerneralmock.config.route.Response;
 import com.wenys.gerneralmock.config.route.info.RoutingInfo;
+import java.nio.charset.StandardCharsets;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -27,29 +29,44 @@ public class RoutingHandler {
 
     public Mono<ServerResponse> route(ServerRequest serverRequest) {
         ServerHttpRequest serverHttpRequest = serverRequest.exchange().getRequest();
-        String path = serverHttpRequest.getPath().value();
-        String header = serverHttpRequest.getHeaders().toString();
-        String queryParam = serverHttpRequest.getQueryParams().toString();
-        log.info(String.format("Path: %s / QueryParam: %s / Header: %s", path, queryParam, header));
-        ServerResponseBuilder serverResponseBuilder = new ServerResponseBuilder();
-        Mono<ServerResponseBuilder> responseMono = Mono.just(serverResponseBuilder);
 
-        for(RoutingInfo.RoutingPolicy policy : routingInfo.getRoutingPolicies()) {
-            if(matchPath(path, policy.getPath())) {
-                Response customResponse = policy.getResponse();
-                if (customResponse != null) {
-                    responseMono = responseMono.flatMap(pServerResponseBuilder -> {
-                        pServerResponseBuilder.responseCode(customResponse.getCode()).responseBody(customResponse.getBody());
-                        return Mono.just(pServerResponseBuilder);
-                    });
-                }
+        return serverHttpRequest.getBody().collectList().flatMap(dataBuffer -> {
+            String path = serverHttpRequest.getPath().value();
+            String header = serverHttpRequest.getHeaders().toString();
+            String queryParam = serverHttpRequest.getQueryParams().toString();
 
-                for(Filter filter : policy.getFilters()) {
-                    responseMono = filter.doFilter(responseMono);
+            StringBuilder rawBody = new StringBuilder();
+            dataBuffer.forEach(data -> {
+                byte[] bytes = new byte[data.readableByteCount()];
+                data.read(bytes);
+                DataBufferUtils.release(data);
+                rawBody.append(new String(bytes, StandardCharsets.UTF_8));
+            });
+
+            log.info(String.format("Path: %s / QueryParam: %s / Header: %s / body : %s", path, queryParam, header, rawBody));
+            ServerResponseBuilder serverResponseBuilder = new ServerResponseBuilder();
+            Mono<ServerResponseBuilder> responseMono = Mono.just(serverResponseBuilder);
+
+            for(RoutingInfo.RoutingPolicy policy : routingInfo.getRoutingPolicies()) {
+                if(matchPath(path, policy.getPath())) {
+                    Response customResponse = policy.getResponse();
+                    if (customResponse != null) {
+                        responseMono = responseMono.flatMap(pServerResponseBuilder -> {
+                            pServerResponseBuilder.responseCode(customResponse.getCode()).responseBody(customResponse.getBody());
+                            return Mono.just(pServerResponseBuilder);
+                        });
+                    }
+
+                    for(Filter filter : policy.getFilters()) {
+                        responseMono = filter.doFilter(responseMono);
+                    }
                 }
             }
-        }
-        return responseMono.flatMap(ServerResponseBuilder::build);
+            return responseMono.flatMap(ServerResponseBuilder::build);
+
+        });
+
+
     }
 
     private boolean matchPath(String urlPath, String rolePath) {
